ade944e29bd0bec144badb4b4f6de4f78d57095b
ivan Poebrezhniuk <vanyapoberejnuk@gmail.com>
2024-04-06 22:00:21 +0300

    feat: login logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import Auth from '@/components/Auth';
-import theme from '@/shared/theme';
-import {
-  Box,
-  ThemeProvider,
-} from '@mui/material';
+import theme from "@/shared/theme";
+import { Box, ThemeProvider } from "@mui/material";
 
-import styles from './page.module.css';
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
-      <Box component="main" className={styles.main}>
-      </Box>
+      <Box component="main" className={styles.main}></Box>
     </ThemeProvider>
   );
 }
--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,18 +1,19 @@
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 
 import {
   Box,
   Button,
   Checkbox,
+  Collapse,
   FormControlLabel,
   FormGroup,
   Stack,
   Switch,
   TextField,
   Typography,
-} from '@mui/material';
+} from "@mui/material";
 
-import { loginSchema } from './constants';
+import { loginSchema } from "./constants";
 
 const styles = {
   container: { width: "100%", maxWidth: 380 },
@@ -26,24 +27,24 @@ const styles = {
 interface ILoginFormValues {
   email: string;
   password: string;
+  use2fa: boolean;
+  code2fa: string;
 }
 
 export default function Login() {
   const {
     isValid,
     values,
-    dirty,
     errors,
     touched,
-    setTouched,
     setFieldValue,
-    handleBlur,
     handleChange,
+    handleSubmit,
   } = useFormik<ILoginFormValues>({
-    initialValues: { email: "", password: "" },
-    onSubmit: () => {},
-    enableReinitialize: true,
-    validateOnMount: true,
+    initialValues: { email: "", password: "", use2fa: false, code2fa: "" },
+    onSubmit: (values) => {
+      alert(JSON.stringify({ isValid, ...values }, null, 2));
+    },
     validateOnChange: true,
     validationSchema: loginSchema,
   });
@@ -55,16 +56,41 @@ export default function Login() {
         <Typography variant="body2">
           We've saved your seat at the winning table.
         </Typography>
-        <TextField label="Email" type="email" />
-        <TextField label="Password" type="password" />
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          onChange={handleChange}
+          value={values.email}
+          helperText={touched.email && errors.email}
+          error={Boolean(Boolean(touched.email && errors.email))}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          onChange={handleChange}
+          value={values.password}
+          helperText={touched.password && errors.password}
+          error={Boolean(Boolean(touched.password && errors.password))}
+        />
         <FormGroup sx={styles.checkbox.container}>
           <FormControlLabel
-            control={<Checkbox defaultChecked />}
+            control={<Checkbox />}
             label="2FA Code"
+            checked={values.use2fa}
+            onChange={() => setFieldValue("use2fa", !values.use2fa)}
           />
         </FormGroup>
-        <TextField />
-        <Button>LOGIN</Button>
+        <Collapse in={values.use2fa}>
+          <TextField
+            id="code2fa"
+            label="Code"
+            onChange={handleChange}
+            value={values.code2fa}
+          />
+        </Collapse>
+        <Button onClick={handleSubmit}>LOGIN</Button>
         <Typography variant="body2" textAlign="center">
           or login with wallet
         </Typography>
